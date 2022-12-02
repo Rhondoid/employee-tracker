1c960e2687d6cda68c2d8a067188a1412297eb5b
Rhonda <110504360+Rhondoid@users.noreply.github.com>
2022-12-02 10:13:26 -0700

    added schema, seeds, js files

---
+++ index.js
@@ -0,0 +1,8 @@
+ //You might also want to make your queries asynchronous. MySQL2 exposes a `.promise()` 
+// function on Connections to upgrade an existing non-Promise connection to use Promises. 
+// To learn more and make your queries asynchronous, refer to the 
+// [npm documentation on MySQL2](https://www.npmjs.com/package/mysql2).
+
+// add tables department, role, employee
+
+
---
+++ sqlfunction.js
@@ -0,0 +1,13 @@
+// You might want to use a separate file that contains functions for performing specific SQL queries you'll need to use.
+//  A constructor function or class could be helpful for organizing these.
+// Try to add some additional functionality to your application, such as the ability to do the following:
+
+// * Update employee managers.
+
+// * View employees by manager.
+
+// * View employees by department.
+
+// * Delete departments, roles, and employees.
+
+// * View the total utilized budget of a department&mdash;in other words, the combined salaries of all employees in that department.
