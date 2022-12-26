f5a96f03dec709eeb9fb60fbfacfd256580d69e5
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2022-12-26 11:50:16 -0700

    added schema update to db

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department(id, dept)
+VALUES ("?"),
+    ("?");
+    
+
+
+
+INSERT INTO role(id, title, salary, dept_id)
+Values("?"),
+    ("?"),
+    ("?"),
+    ("?");
+
+
+
+
+INSERT INTO employee(id, first_name, last_name, role, manager_id),
+VALUES ("?"),
+    ("?"),
+    ("?"),
+    ("?"),
+    ("?");
