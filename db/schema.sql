329e3a54dce279f0d239306affe15a3b7c3a7ea5
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-02-08 21:01:10 -0700

    correcting npm

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE employees_db;
 
 CREATE TABLE department(
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    dept VARCHAR(30) NOT NULL
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
@@ -13,7 +13,7 @@ CREATE TABLE role(
     title VARCHAR(30) NOT NULL,
     salary DECIMAL (14,2) NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee(
@@ -21,8 +21,8 @@ CREATE TABLE employee(
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
     manager_id  INT,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,17 @@
-INSERT INTO department(dept)
-VALUES ("Finance"),
-    ("HR"),
+INSERT INTO department
+    (name)
+VALUES
     ("Sales"),
-    ("Technical");  
+    ("Engineering"),
+    ("Finance"),
+    ("Legal");  
 
 
 
-INSERT INTO role(title, salary, department_id)
-Values("Manager", 80000, 1),
+INSERT INTO role
+    (title, salary, department_id)
+Values
+    ("Manager", 80000, 1),
     ("Sales", 60000, 2),
     ("Marketing", 60000, 3),
     ("Technical", 60000, 4);
@@ -16,8 +20,10 @@ Values("Manager", 80000, 1),
 
 
 
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ("Sandra","Books",3, null),
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES 
+    ("Sandra","Books",3, null),
     ("John","Mitchell",1, null),
     ("Barbara","Strong",2, 1);
     
