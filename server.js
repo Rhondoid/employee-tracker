d80370631051e3d2f50d837b52ee8b45cbee20c7
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-02-21 14:36:23 -0700

    fixed error

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
-//You might also want to make your queries asynchronous. MySQL2 exposes a `.promise()`
-// function on Connections to upgrade an existing non-Promise connection to use Promises.
-// To learn more and make your queries asynchronous, refer to the
-// [npm documentation on MySQL2](https://www.npmjs.com/package/mysql2).
-
-// add tables department, role, employee
-//options: view all departments,
-// view all roles, view all employees, add a department,
-// add a role, add an employee, and update an employee role
 //Main index to start app and prompt choices
 const { prompt } = require("inquirer");
 const db = require("./db");
 const consoleTable = require("console.table");
 
 //function to call promptChoices and initiate questions
-function init(){
+function init() {
   promptChoices();
 }
 
@@ -24,7 +15,7 @@ function promptChoices() {
       name: "choice",
       message: "What do you want to do?",
       choices: [
-        { 
+        {
           name: "View All Employees",
           value: "VIEW_EMPLOYEES",
         },
@@ -40,7 +31,6 @@ function promptChoices() {
           type: "input",
           name: "Add An Employee",
           value: "ADD_EMPLOYEE",
-          
         },
         {
           type: "input",
@@ -76,9 +66,7 @@ function promptChoices() {
         viewRoles();
         break;
       case "ADD_EMPLOYEE":
-        addEmployee(
-          
-        )
+        addEmployee();
         break;
       case "ADD_ROLE":
         addRole();
@@ -97,55 +85,68 @@ function promptChoices() {
 
 function viewEmployees() {
   //methods, making a function for .then
-    db.viewEmployees().then(([rows])=>{
+  db.viewEmployees()
+    .then(([rows]) => {
       let employees = [rows];
       console.table(employees);
     })
-     .then(()=>init())
-};
+    .then(() => init());
+}
 function viewDepartments() {
-    db.viewDepartments().then(([rows])=>{
+  db.viewDepartments()
+    .then(([rows]) => {
       let departments = [rows];
       console.table(departments);
     })
-     .then(()=>init())
-};
+    .then(() => init());
+}
 function viewRoles() {
-    db.viewRoles().then(([rows])=>{
+  db.viewRoles()
+    .then(([rows]) => {
       let roles = [rows];
       console.table(roles);
     })
-     .then(()=>init())
-};
+    .then(() => init());
+}
 function addEmployee() {
-    db.addEmployee().then(([rows])=>{
+  db.addEmployee()
+    .then(([rows]) => {
       let newEmployee = [rows];
       console.table(newEmployee);
     })
-     .then(()=>promptChoices())
-};
+    .then(() => promptChoices());
+}
 function addDepartment() {
-    db.addDepartment().then(([rows])=>{
+  db.addDepartment()
+    .then(([rows]) => {
       let newDepartment = [rows];
       console.table(newDepartment);
     })
-     .then(()=>init())
-};
+    .then(() => init());
+}
 function addRole() {
-    db.addRole().then(([rows])=>{
+  db.addRole()
+    .then(([rows]) => {
       let newRole = [rows];
       console.table(newRole);
     })
-     .then(()=>init())
-};
+    .then(() => init());
+}
 function updateEmployeeRole() {
-    db.updateEmployeeRole().then(([rows])=>{
+  db.updateEmployeeRole()
+    .then(([rows]) => {
       let updatedRole = [rows];
       console.table(updatedRole);
     })
-    .then(()=>init())
-};
-function quit() {
-  process.exit();
+    .then(() => init());
 }
-
+function quit() {
+  this.connection.end(function (err) {
+    if (err) {
+      console.error("Error closing database connection: " + err.message);
+    } else {
+      console.log("Database connection closed gracefully.");
+    }
+    process.exit();
+  });
+}
